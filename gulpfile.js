488aeb981cf427013430b4f531cd5aced3e374a4
Ellington <panpevnev@yandex.com>
2021-12-05 17:38:30 +0300

    added new blocks

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,9 @@ function browsersync() {
 
 
 function styles() {
-	return src('app/scss/style.scss')
+	return src([
+		'app/scss/style.scss',
+	])
 	.pipe(scss({outputStyle: "compressed"}))
 	.pipe(concat('style.min.css'))
 	.pipe(autoprefixer({
@@ -38,7 +40,8 @@ function scripts() {
 	return src([
 		'node_modules/jquery/dist/jquery.js',
 		'node_modules/slick-carousel/slick/slick.js',
-		'app/js/main.js'
+		'node_modules/rateyo/src/jquery.rateyo.js',
+		'app/js/main.js',
 	])
 	.pipe(concat('main.min.js'))
 	.pipe(uglify())
